efce7c69722896c0fdd1d581db585c5bf4450ab3
jasor63 <surhentharchess7gmail.com>
2024-01-28 16:15:22 +0530

    Add available pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"; 
+import About from "./pages/About"; 
+import Profile from "./pages/Profile"; 
+import SignIn from "./pages/SignIn"; 
+import SignUp from "./pages/SignUp"; 
 
 export default function App() {
   return (
-    <div className='text-amber-400'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/profile' element={<Profile />}></Route>
+        <Route path='/signin' element={<SignIn />}></Route>
+        <Route path='/signup' element={<SignUp />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
